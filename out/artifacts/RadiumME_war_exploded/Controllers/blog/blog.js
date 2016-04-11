662d50411598de40efe79b5f1e3ab9d8879cae42
karl <285672259@qq.com>
2016-04-11 19:19:01 +0800

    master

--- out/artifacts/RadiumME_war_exploded/Controllers/blog/blog.js
+++
@@ -1,9 +0,0 @@
-
-// $(".body").children(".content").children("div").each(function(){
-//     var maxH = 0;
-//     $(this).children("div").each(function() {
-//         maxH = $(this).height()>maxH?$(this).height():maxH;
-//     });
-//     console.log(maxH);
-//     $(this).children("div").css({"height":maxH+"px"});
-// });
