b91eaaa4dc1cb8108a05c5eae63e35cd0fe0be26
karl <285672259@qq.com>
2016-04-25 19:12:59 +0800

    master

--- MySpringMVC/web/Framework/addon/nav/nav.js
+++ MySpringMVC/web/Framework/addon/nav/nav.js
@@ -1,3 +1,43 @@
 /**
  * Created by karl on 2016/4/1.
  */
+(function (factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+
+}(function ($) {
+    'use strict';
+    $.fn.nav = (options)=> {
+        return this.each(()=> {
+            nav($(this), options);
+        });
+    };
+
+    let nav = (element, options)=> {
+        let defaults = element.data("setting") || {};
+        let settings = $.extend(defaults, options);
+
+        if (!element.data("init")) {
+            //add class
+            if (!element.hasClass("addon-nav")) {
+                element.addClass("addon-nav");
+            }
+
+            // // build html
+            // element.append(function () {
+            //
+            // });
+            element.data({"init": true});
+        }
+
+
+        return element;
+    }
+
+}));
---
+++ MySpringMVC/web/Modules/login/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by karl on 2016/4/25.
+ */
+require.config({
+    paths: {
+        foo: 'libs/foo-1.1.3'
+    }
+});
