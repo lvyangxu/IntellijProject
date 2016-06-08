d86e17051a16482bf9ab81f0c383934fd031e87b
karl <285672259@qq.com>
2016-06-08 17:58:34 +0800

    master

--- RadiumME/web/Modules/contact_us/js/index.es6
+++ RadiumME/web/Modules/contact_us/js/index.es6
@@ -1,10 +1,33 @@
 {
     //load text
-    let selectors = [".top button", ".recruit-content .small"];
-    let names = ["top-button", "contact-text"];
+    let selectors = [".top button", ".recruit-content1 .big", ".recruit-content1 .small",
+        ".recruit-content2 .big", ".recruit-content2 .small",
+        ".recruit-content3 .big", ".recruit-content3 .small"];
+    let names = ["top-button", "big1", "small1", "big2", "small2", "big3", "small3"];
     let page = "contact-us";
     loadText(page, selectors, names);
     switchLanguage(()=> {
         loadText(page, selectors, names);
     });
+
+    $(".recruit").fit(()=>{
+        let w = $(window).width();
+        if (w < 1330) {
+            let sectionW = (w - 4) / 3;
+            $(".recruit").children("div").css({
+                width: sectionW,
+                height: sectionW
+            });
+            let bigH = $(".recruit").children("div").children(".big").height();
+            let smallH = $(".recruit").children("div").children(".small").height();
+            $(".recruit").children("div").children(".big").css({
+                "padding-top":(sectionW-15-bigH-smallH)/2
+            });
+            $(".recruit").children("div").children(".circle").css({
+                "top":(sectionW-15-bigH-smallH)/2+10
+            });
+        }
+    });
+
+
 }
--- RadiumME/web/Modules/contact_us/js/index.js
+++ RadiumME/web/Modules/contact_us/js/index.js
@@ -3,12 +3,31 @@
 {
     (function () {
         //load text
-        var selectors = [".top button", ".recruit-content .small"];
-        var names = ["top-button", "contact-text"];
+        var selectors = [".top button", ".recruit-content1 .big", ".recruit-content1 .small", ".recruit-content2 .big", ".recruit-content2 .small", ".recruit-content3 .big", ".recruit-content3 .small"];
+        var names = ["top-button", "big1", "small1", "big2", "small2", "big3", "small3"];
         var page = "contact-us";
         loadText(page, selectors, names);
         switchLanguage(function () {
             loadText(page, selectors, names);
+        });
+
+        $(".recruit").fit(function () {
+            var w = $(window).width();
+            if (w < 1330) {
+                var sectionW = (w - 4) / 3;
+                $(".recruit").children("div").css({
+                    width: sectionW,
+                    height: sectionW
+                });
+                var bigH = $(".recruit").children("div").children(".big").height();
+                var smallH = $(".recruit").children("div").children(".small").height();
+                $(".recruit").children("div").children(".big").css({
+                    "padding-top": (sectionW - 15 - bigH - smallH) / 2
+                });
+                $(".recruit").children("div").children(".circle").css({
+                    "top": (sectionW - 15 - bigH - smallH) / 2 + 10
+                });
+            }
         });
     })();
 }
